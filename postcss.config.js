829fc4740df1fa22fe94b0958f909201c600a409
肇 <liuzhao2002cn@outlook.com>
2025-10-19 09:37:41 +0800

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,12 @@
-const postcssImport = require('postcss-import');
+// postcss.config.js
+
+// 确保这里的点 '.' 是半角的
 module。exports = {
-  plugins: [
-    postcssImport({
+  plugins: {
+    // 确保这里的引号 '' 和冒号 : 都是半角的
+    'postcss-import': {
       // 明确告诉 postcss-import，去 'assets/css/' 文件夹里寻找 @import 的文件
-      path: ['assets/css/'] 
-    }),
-    // 如果未来需要 autoprefixer，可以这样添加
-    // require('autoprefixer')({ ...options... }),
-  ]
+      path: ['assets/css/']
+    }
+  }
 };
