dd91dcf1672fb91f675f3ab93180e353e7f37323
肇 <liuzhao2002cn@outlook.com>
2025-10-20 11:08:46 +0800

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
-// postcss.config.js
-
 module.exports = {
   plugins: {
-    // 确保这里的引号 '' 和冒号 : 都是半角的
     'postcss-import': {
-      // 明确告诉 postcss-import，去 'assets/css/' 文件夹里寻找 @import 的文件
       path: ['assets/css/']
     }
   }
